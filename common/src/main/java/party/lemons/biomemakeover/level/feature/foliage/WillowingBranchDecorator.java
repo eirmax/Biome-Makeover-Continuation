5deb154c9f903237c084f8e3bd91e9a76221f46e
eirmax <eirmax8@gmail.com>
2025-09-16 17:37:43 +0700

    WillowingBranches fix

--- common/src/main/java/party/lemons/biomemakeover/level/feature/foliage/WillowingBranchDecorator.java
+++ common/src/main/java/party/lemons/biomemakeover/level/feature/foliage/WillowingBranchDecorator.java
@@ -21,7 +21,7 @@ public class WillowingBranchDecorator extends TreeDecorator {
 
     @Override
     protected TreeDecoratorType<?> type() {
-        return BMFeatures.WILLOWING_BRANCH_DECORATOR.get();
+        return BMFeatures.WILLOWING_BRANCH_DECORATOR.value();
     }
 
     @Override
--- common/src/main/java/party/lemons/biomemakeover/level/generate/foliage/WillowSaplingGenerator.java
+++ common/src/main/java/party/lemons/biomemakeover/level/generate/foliage/WillowSaplingGenerator.java
@@ -4,18 +4,24 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.Nullable;
 import party.lemons.biomemakeover.BiomeMakeover;
 
-public class WillowSaplingGenerator extends AbstractTreeGrower {
+import java.util.Optional;
+
+public class WillowSaplingGenerator {
     public static final ResourceKey<ConfiguredFeature<?,?>> SMALL = ResourceKey.create(Registries.CONFIGURED_FEATURE, BiomeMakeover.ID("swamp/willow"));
 
-    @Nullable
-    @Override
-    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource randomSource, boolean bl)
-    {
-        return SMALL;
-    }
+    public static final TreeGrower WILLOW = new TreeGrower(
+            "willow",
+            0.1F,
+            Optional.empty(),
+            Optional.empty(),
+            Optional.of(SMALL),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.empty()
+    );
 }
