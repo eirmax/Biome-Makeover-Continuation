143018032064c86301105fb0a7384d62371c860e
eirmax <eirmax8@gmail.com>
2025-09-20 21:16:21 +0700

    Enchantment fix part 1

--- common/src/main/java/party/lemons/biomemakeover/init/BMEnchantments.java
+++ common/src/main/java/party/lemons/biomemakeover/init/BMEnchantments.java
@@ -1,18 +1,18 @@
 package party.lemons.biomemakeover.init;
 
 import dev.architectury.registry.registries.DeferredRegister;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import party.lemons.biomemakeover.BMConfig;
 import party.lemons.biomemakeover.BiomeMakeover;
 import party.lemons.biomemakeover.Constants;
 import party.lemons.biomemakeover.item.enchantment.*;
 
-import java.util.function.Supplier;
 
 public class BMEnchantments {
 
@@ -20,16 +20,16 @@ public class BMEnchantments {
     public static final TagKey<Enchantment> ALTAR_CURSE_EXCLUDED = TagKey.create(Registries.ENCHANTMENT, BiomeMakeover.ID("altar_curse_excluded"));
     public static final TagKey<Enchantment> ALTAR_CANT_UPGRADE = TagKey.create(Registries.ENCHANTMENT, BiomeMakeover.ID("altar_cant_upgrade"));
 
-    public static final Supplier<Enchantment> DECAY_CURSE = ENCHANTS.register(BiomeMakeover.ID("decay_curse"), ()->new DecayCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.DECAY));
-    public static final Supplier<Enchantment> INSOMNIA_CURSE = ENCHANTS.register(BiomeMakeover.ID("insomnia_curse"), ()->new InsomniaCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.INSOMNIA));
-    public static final Supplier<Enchantment> CONDUCTIVITY_CURSE = ENCHANTS.register(BiomeMakeover.ID("conductivity_curse"), ()->new ConductivityCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.CONDUCTIVITY));
-    public static final Supplier<Enchantment> ENFEEBLEMENT_CURSE = ENCHANTS.register(BiomeMakeover.ID("enfeeblement_curse"), ()->new EnfeeblementCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.ENFEEBLEMENT));
-    public static final Supplier<Enchantment> DEPTH_CURSE = ENCHANTS.register(BiomeMakeover.ID("depth_curse"), ()->new DepthsCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.DEPTHS));
-    public static final Supplier<Enchantment> FLAMMABILITY_CURSE = ENCHANTS.register(BiomeMakeover.ID("flammability_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.FLAMMABILITY, true, Enchantment.Rarity.UNCOMMON, EnchantmentCategory.ARMOR, new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET}));
-    public static final Supplier<Enchantment> SUFFOCATION_CURSE = ENCHANTS.register(BiomeMakeover.ID("suffocation_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.SUFFOCATION, true, Enchantment.Rarity.UNCOMMON, EnchantmentCategory.ARMOR_HEAD, new EquipmentSlot[]{EquipmentSlot.HEAD}));
-    public static final Supplier<Enchantment> UNWIELDINESS_CURSE = ENCHANTS.register(BiomeMakeover.ID("unwieldiness_curse"), ()->new UnwieldinessCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.UNWIELDINESS));
-    public static final Supplier<Enchantment> INACCURACY_CURSE = ENCHANTS.register(BiomeMakeover.ID("inaccuracy_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.INACCURACY, true, Enchantment.Rarity.UNCOMMON, EnchantmentCategory.BOW, new EquipmentSlot[]{EquipmentSlot.MAINHAND}));
-    public static final Supplier<Enchantment> BUCKLING_CURSE = ENCHANTS.register(BiomeMakeover.ID("buckling_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.BUCKLING, true, Enchantment.Rarity.UNCOMMON, EnchantmentCategory.ARMOR_LEGS, new EquipmentSlot[]{EquipmentSlot.LEGS}));
+    public static final Holder<Enchantment> DECAY_CURSE = ENCHANTS.register(BiomeMakeover.ID("decay_curse"), ()->new DecayCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.DECAY));
+    public static final Holder<Enchantment> INSOMNIA_CURSE = ENCHANTS.register(BiomeMakeover.ID("insomnia_curse"), ()->new InsomniaCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.INSOMNIA));
+    public static final Holder<Enchantment> CONDUCTIVITY_CURSE = ENCHANTS.register(BiomeMakeover.ID("conductivity_curse"), ()->new ConductivityCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.CONDUCTIVITY));
+    public static final Holder<Enchantment> ENFEEBLEMENT_CURSE = ENCHANTS.register(BiomeMakeover.ID("enfeeblement_curse"), ()->new EnfeeblementCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.ENFEEBLEMENT));
+    public static final Holder<Enchantment> DEPTH_CURSE = ENCHANTS.register(BiomeMakeover.ID("depth_curse"), ()->new DepthsCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.DEPTHS));
+    public static final Holder<Enchantment> FLAMMABILITY_CURSE = ENCHANTS.register(BiomeMakeover.ID("flammability_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.FLAMMABILITY, true, Rarity.UNCOMMON, new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET}));
+    public static final Holder<Enchantment> SUFFOCATION_CURSE = ENCHANTS.register(BiomeMakeover.ID("suffocation_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.SUFFOCATION, true, Rarity.UNCOMMON, new EquipmentSlot[]{EquipmentSlot.HEAD}));
+    public static final Holder<Enchantment> UNWIELDINESS_CURSE = ENCHANTS.register(BiomeMakeover.ID("unwieldiness_curse"), ()->new UnwieldinessCurseEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.UNWIELDINESS));
+    public static final Holder<Enchantment> INACCURACY_CURSE = ENCHANTS.register(BiomeMakeover.ID("inaccuracy_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.INACCURACY, true, Rarity.UNCOMMON, new EquipmentSlot[]{EquipmentSlot.MAINHAND}));
+    public static final Holder<Enchantment> BUCKLING_CURSE = ENCHANTS.register(BiomeMakeover.ID("buckling_curse"), ()->new BMEnchantment(()->BMConfig.INSTANCE.enchantmentConfig.BUCKLING, true, Rarity.UNCOMMON, new EquipmentSlot[]{EquipmentSlot.LEGS}));
 
     public static void init() {
         ENCHANTS.register();
