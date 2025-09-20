6496a3796888b74a6179879808882b799577a7ca
eirmax <eirmax8@gmail.com>
2025-09-20 21:23:44 +0700

    fix last mixins

--- common/src/main/java/party/lemons/biomemakeover/crafting/witch/data/reward/PotionQuestRewardItem.java
+++ common/src/main/java/party/lemons/biomemakeover/crafting/witch/data/reward/PotionQuestRewardItem.java
@@ -2,22 +2,23 @@ package party.lemons.biomemakeover.crafting.witch.data.reward;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.PotionContents;
 import party.lemons.biomemakeover.util.RandomUtil;
 
 public class PotionQuestRewardItem extends QuestRewardItem
 {
-	private final Potion potion;
+	private final Holder<Potion> potion;
 
 	public PotionQuestRewardItem(Potion potion)
 	{
-		this.potion = potion;
+		this.potion = (Holder<Potion>) potion;
 	}
 
 	@Override
@@ -35,12 +36,12 @@ public class PotionQuestRewardItem extends QuestRewardItem
 		} else {
 			it = Items.SPLASH_POTION;
 		}
-		return PotionUtils.setPotion(new ItemStack(it), potion);
+		return PotionContents.createItemStack(new ItemStack(it).getItem(), potion);
 	}
 
 	public static final Codec<PotionQuestRewardItem> CODEC = RecordCodecBuilder.create(instance ->
 			instance.group(
-							BuiltInRegistries.POTION.byNameCodec().fieldOf("potion").forGetter(i->i.potion)
+							BuiltInRegistries.POTION.byNameCodec().fieldOf("potion").forGetter(i-> i.potion.value())
 					)
 					.apply(instance, PotionQuestRewardItem::new));
 }
