52c57fa0f8f9d9a44f02dda5b9b72548fb203e38
eirmax <eirmax8@gmail.com>
2025-09-24 23:29:19 +0700

    Fix Cursing tags

--- common/src/main/java/party/lemons/biomemakeover/item/Cursing.java
+++ common/src/main/java/party/lemons/biomemakeover/item/Cursing.java
@@ -1,29 +1,30 @@
 package party.lemons.biomemakeover.item;
 
 import com.google.common.collect.Lists;
+import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.EnchantmentTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.level.Level;
 import party.lemons.biomemakeover.BMConfig;
 import party.lemons.biomemakeover.init.BMEnchantments;
 import party.lemons.biomemakeover.util.RandomUtil;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Cursing
 {
-    private static final List<Enchantment> curses = Lists.newArrayList();
+    private static final List<Holder<Enchantment>> curses = Lists.newArrayList();
 
     public static boolean isValidForCurse(ItemStack stack)
     {
@@ -31,25 +32,44 @@ public class Cursing
 
         if(stack.getItem() == Items.BOOK) return true;
 
-        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
-        if(enchantments.isEmpty() || (stack.hasTag() && stack.getTag().contains("BMCursed"))) return false;
+        ItemEnchantments enchantments = stack.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+
+        if(stack.has(DataComponents.CUSTOM_DATA) &&
+                stack.get(DataComponents.CUSTOM_DATA).copyTag().getBoolean("BMCursed")) {
+            return false;
+        }
+
+        if(enchantments.isEmpty()) return false;
 
         boolean hasNewCompatibleCurse = false;
-        for(Enchantment enchantment : enchantments.keySet())
+        for(Holder<Enchantment> enchantmentHolder : enchantments.keySet())
         {
-            if(!BuiltInRegistries.ENCHANTMENT.wrapAsHolder(enchantment).is(BMEnchantments.ALTAR_CANT_UPGRADE) && enchantment.getMaxLevel() > 1 && !enchantment.isCurse() && (!BMConfig.INSTANCE.strictAltarCursing || enchantments.get(enchantment) < enchantment.getMaxLevel() + 1)) return true;
+            Enchantment enchantment = enchantmentHolder.value();
+            if(!enchantmentHolder.is(BMEnchantments.ALTAR_CANT_UPGRADE) &&
+                    enchantment.getMaxLevel() > 1 &&
+                    !enchantmentHolder.is(EnchantmentTags.CURSE) &&
+                    (!BMConfig.INSTANCE.strictAltarCursing || enchantments.getLevel(enchantmentHolder) < enchantment.getMaxLevel() + 1)) {
+                return true;
+            }
 
-            if(enchantment.isCurse() && enchantment.canEnchant(stack) && !enchantments.containsKey(enchantment))
+            if(enchantmentHolder.is(EnchantmentTags.CURSE) &&
+                    enchantment.canEnchant(stack) &&
+                    !enchantments.keySet().contains(enchantmentHolder)) {
                 hasNewCompatibleCurse = true;
+            }
         }
         return hasNewCompatibleCurse;
     }
 
-    public static Enchantment getRandomCurse(RegistryAccess registryAccess, RandomSource random)
+    public static Holder<Enchantment> getRandomCurse(RegistryAccess registryAccess, RandomSource random)
     {
         if(curses.isEmpty())
         {
-            curses.addAll(registryAccess.registryOrThrow(Registries.ENCHANTMENT).stream().filter(e->e.isCurse() && !BuiltInRegistries.ENCHANTMENT.wrapAsHolder(e).is(BMEnchantments.ALTAR_CURSE_EXCLUDED)).toList());
+            curses.addAll(registryAccess.registryOrThrow(Registries.ENCHANTMENT)
+                    .holders()
+                    .filter(holder -> holder.is(EnchantmentTags.CURSE) &&
+                            !holder.is(BMEnchantments.ALTAR_CURSE_EXCLUDED))
+                    .toList());
         }
         if(curses.isEmpty())
             return null;
@@ -63,29 +83,36 @@ public class Cursing
         {
             if(stack.getItem() == Items.BOOK)
             {
-                ItemStack newStack = new ItemStack(Items.ENCHANTED_BOOK);
-                Enchantment curse = Cursing.getRandomCurse(level.registryAccess(), level.random);
+                Holder<Enchantment> curse = Cursing.getRandomCurse(level.registryAccess(), level.random);
                 if(curse == null)
                     return ItemStack.EMPTY;
-                EnchantedBookItem.addEnchantment(newStack, new EnchantmentInstance(curse, 1));
 
-                return newStack;
+                return EnchantedBookItem.createForEnchantment(new EnchantmentInstance(curse, 1));
             }
 
-            Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(stack);
-            List<Enchantment> validEnchants = enchantments.keySet().stream().filter(
-                    e -> !BuiltInRegistries.ENCHANTMENT.wrapAsHolder(e).is(BMEnchantments.ALTAR_CANT_UPGRADE) && e.getMaxLevel() > 1 && !e.isCurse() && (!BMConfig.INSTANCE.strictAltarCursing || enchantments.get(e) < e.getMaxLevel() + 1)
+            ItemEnchantments currentEnchantments = stack.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+            ItemEnchantments.Mutable enchantments = new ItemEnchantments.Mutable(currentEnchantments);
+
+            List<Holder<Enchantment>> validEnchants = currentEnchantments.keySet().stream().filter(
+                    holder -> !holder.is(BMEnchantments.ALTAR_CANT_UPGRADE) &&
+                            holder.value().getMaxLevel() > 1 &&
+                            !holder.is(EnchantmentTags.CURSE) &&
+                            (!BMConfig.INSTANCE.strictAltarCursing ||
+                                    currentEnchantments.getLevel(holder) < holder.value().getMaxLevel() + 1)
             ).toList();
 
-            Enchantment toUpgrade = validEnchants.get(random.nextInt(validEnchants.size()));
-            enchantments.put(toUpgrade, enchantments.get(toUpgrade) + 1);
+            if(validEnchants.isEmpty()) return ItemStack.EMPTY;
 
-            Enchantment curse = getRandomCurse(level.registryAccess(), random);
+            Holder<Enchantment> toUpgrade = validEnchants.get(random.nextInt(validEnchants.size()));
+            int currentLevel = enchantments.getLevel(toUpgrade);
+            enchantments.set(toUpgrade, currentLevel + 1);
+
+            Holder<Enchantment> curse = getRandomCurse(level.registryAccess(), random);
             if(curse == null)
                 return ItemStack.EMPTY;
 
-            int attempts = 0;   //Attempts is to stop a potential infinite loop, if code is up to this point we SHOULD have a curse that's compatible, we gonna brute force it at this point lol
-            while(enchantments.containsKey(curse) || !curse.canEnchant(stack))
+            int attempts = 0;
+            while(enchantments.keySet().contains(curse) || !curse.value().canEnchant(stack))
             {
                 curse = getRandomCurse(level.registryAccess(), random);
                 if(curse == null)
@@ -99,27 +126,43 @@ public class Cursing
                 }
             }
 
-            //Brute force tactic
             if(curse == null)
             {
-                for(Enchantment enchantment : level.registryAccess().registryOrThrow(Registries.ENCHANTMENT).stream().sorted((e, e1)-> RandomUtil.randomRange(-1, 1)).collect(Collectors.toList()))
+                List<Holder<Enchantment>> allEnchantments = level.registryAccess()
+                        .registryOrThrow(Registries.ENCHANTMENT)
+                        .holders()
+                        .sorted((e, e1) -> RandomUtil.randomRange(-1, 1))
+                        .collect(Collectors.toList());
+
+                for(Holder<Enchantment> enchantmentHolder : allEnchantments)
                 {
-                    if(enchantment.isCurse() && enchantment.canEnchant(stack) && !enchantments.containsKey(enchantment))
-                        curse = enchantment;
+                    if(enchantmentHolder.is(EnchantmentTags.CURSE) &&
+                            enchantmentHolder.value().canEnchant(stack) &&
+                            !enchantments.keySet().contains(enchantmentHolder))
+                    {
+                        curse = enchantmentHolder;
+                        break;
+                    }
                 }
             }
 
             if(curse == null)
                 return ItemStack.EMPTY;
 
-            int curseLevel = curse.getMaxLevel() == 1 ? 1 : RandomUtil.randomRange(curse.getMinLevel(), curse.getMaxLevel());
-            enchantments.put(curse, curseLevel);
-            CompoundTag tag = stack.getOrCreateTag();
-            tag.putBoolean("BMCursed", true);
-            stack.setTag(tag);
-            stack.setRepairCost(39);
+            Enchantment curseEnchantment = curse.value();
+            int curseLevel = curseEnchantment.getMaxLevel() == 1 ? 1 :
+                    RandomUtil.randomRange(curseEnchantment.getMinLevel(), curseEnchantment.getMaxLevel());
+            enchantments.set(curse, curseLevel);
 
-            EnchantmentHelper.setEnchantments(enchantments, stack);
+            stack.set(DataComponents.CUSTOM_DATA,
+                    stack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY)
+                            .update(tag -> tag.putBoolean("BMCursed", true)));
+
+            stack.set(DataComponents.REPAIR_COST, 39);
+
+            // Apply enchantments
+            stack.set(DataComponents.ENCHANTMENTS, enchantments.toImmutable());
+
             return stack;
         }
 
